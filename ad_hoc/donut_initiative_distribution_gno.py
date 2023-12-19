f90e060c52a6c3ae3d7230acadb3ecbb7457c551
Matthew <matthew.gimbl@gmail.com>
2023-12-19 17:44:24 -0600

    clean up dict to make it easier to add items in the future

--- ad_hoc/donut_initiative_distribution_gno.py
+++ ad_hoc/donut_initiative_distribution_gno.py
@@ -27,111 +27,80 @@ if __name__ == '__main__':
     with open(os.path.normpath("../contracts/gno_contrib_abi.json"), 'r') as f:
         gno_contrib_abi = json.load(f)
 
-        w3 = Web3()
-        distribute_contract = w3.eth.contract(address=w3.to_checksum_address(GNO_DISTRIBUTE_CONTRACT_ADDRESS),
-                                              abi=gno_distribute_abi)
+    w3 = Web3()
+    distribute_contract = w3.eth.contract(address=w3.to_checksum_address(GNO_DISTRIBUTE_CONTRACT_ADDRESS),
+                                          abi=gno_distribute_abi)
 
-        gno_contrib_contract = w3.eth.contract(address=w3.to_checksum_address(GNO_CONTRIB_CONTRACT_ADDRESS),
-                                               abi=gno_contrib_abi)
+    gno_contrib_contract = w3.eth.contract(address=w3.to_checksum_address(GNO_CONTRIB_CONTRACT_ADDRESS),
+                                           abi=gno_contrib_abi)
 
-        # this is for the following two initiatives:
-        # 1 - https://www.reddit.com/r/ethtrader/comments/16sq29i/donut_initiative_improve_mydonuts_karmadonut/
-        # 2 - https://www.reddit.com/r/ethtrader/comments/17tjmmu/donut_initiative_rewarding_the_development_of/
+    # this is for the following two initiatives:
+    # 1 - https://www.reddit.com/r/ethtrader/comments/16sq29i/donut_initiative_improve_mydonuts_karmadonut/
+    # 2 - https://www.reddit.com/r/ethtrader/comments/17tjmmu/donut_initiative_rewarding_the_development_of/
 
-        donuts_awarded = [
-            {
-                "to": "mattg1981",
-                "amount": 100000,
-                "reason": "donut initiative",
-                "address": None
-            },
-            {
-                "to": "reddito321",
-                "amount": 150000,
-                "reason": "donut initiative",
-                "address": None
-            },
-            {
-                "to": "mattg1981",
-                "amount": 10000,
-                "reason": "reward organizer",
-                "address": None
-            }
-        ]
+    awards = [
+        {
+            "to": "mattg1981",
+            "donut": 100000,
+            "contrib": 20000,
+            "reason": "donut initiative"
+        },
+        {
+            "to": "reddito321",
+            "donut": 150000,
+            "contrib": 150000,
+            "reason": "donut initiative"
+        },
+        {
+            "to": "mattg1981",
+            "donut": 10000,
+            "contrib": 2000,
+            "reason": "reward organizer"
+        }
+    ]
 
-        contrib_awarded = [
-            {
-                "to": "mattg1981",
-                "amount": 20000,
-                "reason": "donut initiative",
-                "address": None
-            },
-            {
-                "to": "reddito321",
-                "amount": 150000,
-                "reason": "donut initiative",
-                "address": None
-            },
-            {
-                "to": "mattg1981",
-                "amount": 2000,
-                "reason": "reward organizer",
-                "address": None
-            }
-        ]
+    # get users file that will be used for any user <-> address lookups
+    users = json.load(request.urlopen(f"https://ethtrader.github.io/donut.distribution/users.json"))
 
-        # get users file that will be used for any user <-> address lookups
-        users = json.load(request.urlopen(f"https://ethtrader.github.io/donut.distribution/users.json"))
+    # map awardee addresses
+    for award in awards:
+        user = next((u for u in users if u['username'].lower() == award['to'].lower()),None)
 
-        # map donut awardee addresses
-        for d in donuts_awarded:
-            user = next((u for u in users if u['username'].lower() == d['to'].lower()),None)
+        if not user:
+            print(f"donut reward user not found, ensure you typed the name correctly: [{award['to']}]")
+            exit(4)
 
-            if not user:
-                print(f"donut reward user not found, ensure you typed the name correctly: [{d['to']}]")
+        award["address"] = user["address"]
 
-            d["address"] = user["address"]
-
-        # map contrib awardee addresses
-        for c in contrib_awarded:
-            user = next((u for u in users if u['username'].lower() == c['to'].lower()),None)
-
-            if not user:
-                print(f"contrib user not found, ensure you typed the name correctly: [{d['to']}]")
-
-            c["address"] = user["address"]
-
-        # encode the donut data
-        distribute_contract_data = distribute_contract.encodeABI("distribute", [
-                    [w3.to_checksum_address(d['address']) for d in donuts_awarded if float(d['amount']) > 0],
-                    [w3.to_wei(d['amount'], 'ether') for d in donuts_awarded if float(d['amount']) > 0],
-                    w3.to_checksum_address(GNO_DONUT_CONTRACT_ADDRESS)
-                ])
-
-        # encode the contrib data
-        contrib_contract_data = gno_contrib_contract.encodeABI("mintMany", [
-                [w3.to_checksum_address(c['address']) for c in contrib_awarded if float(c['amount']) > 0],
-                [w3.to_wei(c['amount'], 'ether') for c in contrib_awarded if float(c['amount']) > 0]
+    # encode the donut data
+    distribute_contract_data = distribute_contract.encodeABI("distribute", [
+                [w3.to_checksum_address(a['address']) for a in awards if float(a['donut']) > 0],
+                [w3.to_wei(a['donut'], 'ether') for a in awards if float(a['donut']) > 0],
+                w3.to_checksum_address(GNO_DONUT_CONTRACT_ADDRESS)
             ])
 
-        transactions = [
-            SafeTx(to=w3.to_checksum_address(GNO_DISTRIBUTE_CONTRACT_ADDRESS), value=0, data=distribute_contract_data),
-            SafeTx(to=w3.to_checksum_address(GNO_CONTRIB_CONTRACT_ADDRESS), value=0, data=contrib_contract_data),
-        ]
+    # encode the contrib data
+    contrib_contract_data = gno_contrib_contract.encodeABI("mintMany", [
+            [w3.to_checksum_address(a['address']) for a in awards if float(a['contrib']) > 0],
+            [w3.to_wei(a['contrib'], 'ether') for a in awards if float(a['contrib']) > 0]
+        ])
 
-        # build the tx builder json
-        tx = build_tx_builder_json(f"donut-initiative awards", transactions)
+    transactions = [
+        SafeTx(to=w3.to_checksum_address(GNO_DISTRIBUTE_CONTRACT_ADDRESS), value=0, data=distribute_contract_data),
+        SafeTx(to=w3.to_checksum_address(GNO_CONTRIB_CONTRACT_ADDRESS), value=0, data=contrib_contract_data),
+    ]
 
-        datetime_string = datetime.now().strftime('%m-%d-%Y')
+    # build the tx builder json
+    tx = build_tx_builder_json(f"donut-initiative awards", transactions)
 
-        # save the tx builder json to file to be uploaded
-        output_location = f"../out/ad_hoc/donut-initiative_{datetime_string}.json"
+    # save the tx builder json to file to be uploaded
+    output_location = f"../out/ad_hoc/donut-initiative_{datetime.now().strftime('%m-%d-%Y')}.json"
 
-        if os.path.exists(output_location):
-            os.remove(output_location)
+    if os.path.exists(output_location):
+        os.remove(output_location)
 
-        with open(os.path.normpath(output_location), 'w') as f:
-            json.dump(tx, f, indent=4)
+    with open(os.path.normpath(output_location), 'w') as f:
+        json.dump(tx, f, indent=4)
 
 
 
