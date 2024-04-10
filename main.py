fe391407f62be4ce8da1fa7920513fe021adfe29
Matthew Gimbl <matthew.gimbl@gmail.com>
2024-04-10 08:58:18 -0500

    calculate round dynamically if one is not passed in

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import base64
 import json
 import logging
 import sys
 
 from logging.handlers import RotatingFileHandler
 from os import path, makedirs
+from urllib import request
 
 from dotenv import load_dotenv
 from distribution_tasks import *
@@ -18,9 +20,29 @@ if __name__ == '__main__':
     with open(path.normpath("config.json"), 'r') as f:
         config = json.load(f)
 
+    # if a round number is not passed into the program, grab the round from github
+    if len(sys.argv) == 1:
+        git_files = json.load(request.urlopen("https://api.github.com/repos/mattg1981/donut-bot-output/git/trees/main"
+                                              "?recursive=1"))
+
+        dist_rounds = [x for x in sorted(git_files["tree"], key=lambda x: x['path'])
+                       if 'distribution_rounds/distribution_round' in x['path']]
+
+        dist_round = dist_rounds[len(dist_rounds) - 2]
+        dist_round_data = json.load(request.urlopen(dist_round['url']))
+        if dist_round_data['encoding'] != 'base64':
+            print("The distribution round is not base64.  Correct this or supply a distribution round.")
+        content = json.loads(base64.b64decode(dist_round_data['content']))
+        round = int(content[0]['distribution_round'])
+        pass
+
+    else:
+        round = int(sys.argv[1])
+
+
     # create round_{} folder so that we can put the log in there
     base_dir = path.dirname(path.abspath(__file__))
-    log_dir = path.join(base_dir, f"./out/round_{int(sys.argv[1])}/logs")
+    log_dir = path.join(base_dir, f"./out/round_{round}/logs")
     makedirs(path.normpath(log_dir), exist_ok=True)
 
     # set up logging
@@ -29,7 +51,7 @@ if __name__ == '__main__':
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
 
-    file_handler = RotatingFileHandler(path.normpath(path.join(log_dir, f"distribution_{int(sys.argv[1])}.log")), maxBytes=100000000, backupCount=4)
+    file_handler = RotatingFileHandler(path.normpath(path.join(log_dir, f"distribution_{round}.log")), maxBytes=100000000, backupCount=4)
     file_handler.setFormatter(formatter)
     console_handler = logging.StreamHandler()
     logger.addHandler(file_handler)
@@ -51,7 +73,7 @@ if __name__ == '__main__':
 
     pre_cache_run = "--build-cache" in sys.argv
 
-    pipeline_config = {'step': 1, 'round': int(sys.argv[1]), 'build-cache': pre_cache_run}
+    pipeline_config = {'step': 1, 'round': round, 'build-cache': pre_cache_run}
     for task in tasks:
         pipeline_config = task.process(pipeline_config)
         pipeline_config['step'] += 1
