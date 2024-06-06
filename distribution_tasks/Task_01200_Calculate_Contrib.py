e7d2a26a4b17f3024ebb43b566670ececdb87428
Matthew Gimbl <matthew.gimbl@gmail.com>
2024-06-06 14:03:07 -0500

    add post of the week, change how voter contrib is calculated

--- distribution_tasks/Task_01200_Calculate_Contrib.py
+++ distribution_tasks/Task_01200_Calculate_Contrib.py
@@ -13,12 +13,13 @@ class CalculateContribDistributionTask(DistributionTask):
         super().process(pipeline_config)
         self.logger.info(f"begin task [step: {super().current_step}] [file: {os.path.basename(__file__)}]")
 
-        voter_data = super().get_current_document_version(pipeline_config['voter'])
+        # voter_data = super().get_current_document_version(pipeline_config['voter'])
         tip_bonus_data = super().get_current_document_version(pipeline_config['tipping_bonus'])
         mod_rewards = super().get_current_document_version(pipeline_config['mod_rewards'])
         organizer_rewards = super().get_current_document_version(pipeline_config['organizers'])
         base_distribution = super().get_document_version('distribution', 0)
         current_distribution = super().get_current_document_version('distribution')
+        post_of_the_week_winners = super().get_current_document_version('post_of_the_week') or []
 
         contrib = []
 
@@ -27,18 +28,31 @@ class CalculateContribDistributionTask(DistributionTask):
             mod = next((t for t in mod_rewards if t['username'].lower() == d['username'].lower()), None)
             org = next((o for o in organizer_rewards if o['username'].lower() == d['username'].lower()), None)
             base_record = next((b for b in base_distribution if b['username'].lower() == d['username'].lower()), None)
+            post_of_the_week_awards = [p for p in post_of_the_week_winners if p['username'].lower() == d['username'].lower()]
 
-            voter = None
-            if voter_data:
-                voter = next((v for v in voter_data if v['username'].lower() == d['username'].lower()), None)
+            # voter = None
+            # if voter_data:
+            #     voter = next((v for v in voter_data if v['username'].lower() == d['username'].lower()), None)
+
+            post_of_the_week_contrib = 0
+            for award in post_of_the_week_awards:
+                if award['rank'] == 1:
+                    post_of_the_week_contrib += 5000
+                elif award['rank'] == 2:
+                    post_of_the_week_contrib += 3000
+                elif award['rank'] == 3:
+                    post_of_the_week_contrib += 1500
+                else:
+                    post_of_the_week_contrib += 500
 
             tip_bonus = (tip_bonus and tip_bonus['points']) or 0
             mod = (mod and mod['points']) or 0
             org = (org and org['points']) or 0
             base = (base_record and base_record['points'] or 0)
-            voter = (voter and voter['points']) or 0
+            # voter = (voter and voter['points']) or 0
+            voter = Decimal(d['points_after_bonus']) - Decimal(d['points'])
 
-            contrib_points = Decimal(tip_bonus) + Decimal(mod) + Decimal(org) + Decimal(base) + Decimal(voter)
+            contrib_points = Decimal(tip_bonus) + Decimal(mod) + Decimal(org) + Decimal(base) + Decimal(voter) + Decimal(post_of_the_week_contrib)
             contrib_points = max(contrib_points, Decimal(0))
 
             contrib.append({
@@ -48,7 +62,8 @@ class CalculateContribDistributionTask(DistributionTask):
                 'mod': mod,
                 'org': org,
                 'tip_bonus': tip_bonus,
-                'voter': voter
+                'voter': voter,
+                'post_of_the_week': post_of_the_week_contrib
             })
 
         super().save_document_version(contrib, 'contrib')
