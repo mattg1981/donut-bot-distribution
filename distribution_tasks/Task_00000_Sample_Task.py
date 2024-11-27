885b10dc1b802ab80c08a7cda651128c317dcecd
Matthew Gimbl <matthew.gimbl@gmail.com>
2024-11-27 13:51:46 -0600

    additional comments

--- distribution_tasks/Task_00000_Sample_Task.py
+++ distribution_tasks/Task_00000_Sample_Task.py
@@ -12,7 +12,7 @@ class SampleDistributionTask(DistributionTask):
     def process(self, pipeline_config):
         self.logger.info("begin task")
 
-        # important - this call loads the configuration so that you can user properties such as:
+        # important - this call loads the configuration so that you can use properties such as:
         # super().distribution_round
         # super().working_dir
         super().process(pipeline_config)
@@ -21,6 +21,7 @@ class SampleDistributionTask(DistributionTask):
 
         self.logger.info("end task")
 
+        # add results to the pipeline result. These results can be accessed by other tasks that run after this one.
         return super().update_pipeline(pipeline_config, {
             'additional_key_to_be_passed_downtstream': 'value',
         })
