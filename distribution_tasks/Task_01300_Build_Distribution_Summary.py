c240402e8616594058e8a8b90d7438ae0d63e968
mattg1981 <matthew.gimbl@gmail.com>
2025-05-09 08:38:08 -0500

    formatting change

--- distribution_tasks/Task_01300_Build_Distribution_Summary.py
+++ distribution_tasks/Task_01300_Build_Distribution_Summary.py
@@ -1,6 +1,6 @@
 import os
 
-from decimal import Decimal
+# from decimal import Decimal
 from distribution_tasks.distribution_task import DistributionTask
 
 
@@ -11,45 +11,78 @@ class BuildSummaryDistributionTask(DistributionTask):
 
     def process(self, pipeline_config):
         super().process(pipeline_config)
-        self.logger.info(f"begin task [step: {super().current_step}] [file: {os.path.basename(__file__)}]")
+        self.logger.info(
+            f"begin task [step: {super().current_step}] [file: {os.path.basename(__file__)}]"
+        )
 
-        distribution_data = super().get_current_document_version(pipeline_config['distribution'])
-        offchain_data = super().get_current_document_version(pipeline_config['offchain_data'])
-        contrib_data = super().get_current_document_version('contrib')
+        distribution_data = super().get_current_document_version(
+            pipeline_config["distribution"]
+        )
+        offchain_data = super().get_current_document_version(
+            pipeline_config["offchain_data"]
+        )
+        contrib_data = super().get_current_document_version("contrib")
         # voter_data = super().get_current_document_version(pipeline_config['voter'])
         # tip_bonus_data = super().get_current_document_version(pipeline_config['tipping_bonus'])
-        #mod_rewards = super().get_current_document_version(pipeline_config['mod_rewards'])
-        #organizer_rewards = super().get_current_document_version(pipeline_config['organizers'])
-        base_distribution_file = super().get_document_version('distribution', 0)
-        user_data = super().get_current_document_version('users')
-        allocation = super().get_current_document_version('distribution_allocation')[0]
+        # mod_rewards = super().get_current_document_version(pipeline_config['mod_rewards'])
+        # organizer_rewards = super().get_current_document_version(pipeline_config['organizers'])
+        # base_distribution_file = super().get_document_version('distribution', 0)
+        user_data = super().get_current_document_version("users")
+        allocation = super().get_current_document_version("distribution_allocation")[0]
 
         distribution_summary = []
-        
+
         # ETIP - 121
         # https://snapshot.box/#/s:ethtraderdao.eth/proposal/0x8bf37aad048618d6869b280e513d34cc148518de55b5047adfbdcd8549c22577
-        max_points = (float(allocation['posts']) + float(allocation['comments'])) * .05
+        # each distribution has a set amount (allocation['posts'] +  allocation['comments'] = 850k [at time of writing]) of donuts to be distributed.
+        # The max points is 5% of that amount.
+        max_points = (float(allocation["posts"]) + float(allocation["comments"])) * 0.05
+
+        # ETIP - 121?
+        # since donuts are capped at 5% of the total distribution, as well as other cases such as users that are banned
+        # not all of the 850k donuts are distributed.  Those unallocated donuts are to be burned.
+        distribution_summary_total = 0
 
         for d in distribution_data:
+            offchain = next(
+                (
+                    o
+                    for o in offchain_data
+                    if o["user"].lower() == d["username"].lower()
+                ),
+                None,
+            )
+            # mod = next((t for t in mod_rewards if t['username'].lower() == d['username'].lower()), None)
+            # org = next((o for o in organizer_rewards if o['username'].lower() == d['username'].lower()), None)
+            contrib_record = next(
+                (
+                    c
+                    for c in contrib_data
+                    if c["username"].lower() == d["username"].lower()
+                ),
+                None,
+            )
+            user = next(
+                (
+                    u
+                    for u in user_data
+                    if u["username"].lower() == d["username"].lower()
+                ),
+                None,
+            )
 
-            offchain = next((o for o in offchain_data if o['user'].lower() == d['username'].lower()), None)
-            #mod = next((t for t in mod_rewards if t['username'].lower() == d['username'].lower()), None)
-            #org = next((o for o in organizer_rewards if o['username'].lower() == d['username'].lower()), None)
-            contrib_record = next((c for c in contrib_data if c['username'].lower() == d['username'].lower()), None)
-            user = next((u for u in user_data if u['username'].lower() == d['username'].lower()), None)
-
-            comment_score = float(d['comment_score'])
-            if not int(d['eligible_comments']):
+            comment_score = float(d["comment_score"])
+            if not int(d["eligible_comments"]):
                 comment_score = 0
 
-            post_score = float(d['post_score'])
-            if not int(d['eligible_posts']):
+            post_score = float(d["post_score"])
+            if not int(d["eligible_posts"]):
                 post_score = 0
 
             points = comment_score + post_score
 
             # voting = max(float(d['points_after_bonus']), 0) - max(float(d['points']), 0)
-            voting = float(d['voter_bonus_comments']) + float(d['voter_bonus_posts'])
+            voting = float(d["voter_bonus_comments"]) + float(d["voter_bonus_posts"])
             points += voting
 
             # if mod:
@@ -64,67 +97,80 @@ class BuildSummaryDistributionTask(DistributionTask):
             # else:
             #     organizer = 0
 
-            contrib = float((contrib_record and contrib_record['contrib']) or 0)
-            pay2post = float(d['pay2post_after_bonus']) or 0
+            contrib = float((contrib_record and contrib_record["contrib"]) or 0)
+            pay2post = float(d["pay2post_after_bonus"]) or 0
             points -= pay2post
 
             if offchain:
-                offchain_tips = float(offchain['tips'])
+                offchain_tips = float(offchain["tips"])
                 if points <= 0:
-                    points = float(offchain['points'])
+                    points = float(offchain["points"])
                 else:
-                    points += float(offchain['points'])
-                funded = float(offchain['funded'])
+                    points += float(offchain["points"])
+                funded = float(offchain["funded"])
             else:
                 offchain_tips = 0
                 funded = 0
 
-            if not d['eligible_comments'] and not d['eligible_posts']:
+            if not d["eligible_comments"] and not d["eligible_posts"]:
                 # -- mattg1981 -- ineligible users can now send and receive tips
                 # but will not receive their base score or any bonuses as well as 0 contrib
 
                 # return their funded amount + tips
                 if offchain:
-                    points = float(offchain['points'])
+                    points = float(offchain["points"])
                 else:
                     points = 0
                 contrib = 0
-                
+
             # remove negative values
-            points = max(round(points, 4), 0)            
+            points = max(round(points, 4), 0)
 
-            distribution_summary.append({
-                'username': d['username'],
-                'points': min(points, max_points),
-                'contrib': round(contrib, 4),
-                # 'base': round(float(base), 4),
+            # cap the points at 5% of the total distribution
+            points = min(points, max_points)
+            distribution_summary_total += points
 
-                # 'base': Decimal(d['points_after_bonus']),
-                # 'comment_score': d['comment_score_after_bonus'],
-                # 'post_score': d['post_score_after_bonus'],
+            distribution_summary.append(
+                {
+                    "username": d["username"],
+                    "points": points,
+                    "contrib": round(contrib, 4),
+                    # 'base': round(float(base), 4),
+                    # 'base': Decimal(d['points_after_bonus']),
+                    # 'comment_score': d['comment_score_after_bonus'],
+                    # 'post_score': d['post_score_after_bonus'],
+                    "comment_score": round(
+                        comment_score, 4
+                    ),  # from the value in Build_Comment2Vote
+                    "post_score": round(
+                        post_score, 4
+                    ),  # from the value in Build_Comment2Vote
+                    "offchain_tips": round(offchain_tips, 4),
+                    "funded": round(funded, 4),
+                    "voting": round(voting, 4),
+                    # 'donut_upvoter': donut_upvoter,
+                    # 'quad_rank': quad_rank,
+                    #'moderator': round(moderator, 4),
+                    #'organizer': round(organizer, 4),
+                    "pay2post": round(pay2post * -1, 4),
+                    "eligible_comments": d["eligible_comments"],
+                    "eligible_posts": d["eligible_posts"],
+                    "eligibility_reason": d["eligibility_reason"],
+                    "address": user["address"],
+                }
+            )
 
-                'comment_score': round(comment_score, 4),  # from the value in Build_Comment2Vote
-                'post_score': round(post_score, 4),  # from the value in Build_Comment2Vote
-
-                'offchain_tips': round(offchain_tips, 4),
-                'funded': round(funded, 4),
-                'voting': round(voting, 4),
-                # 'donut_upvoter': donut_upvoter,
-                # 'quad_rank': quad_rank,
-                #'moderator': round(moderator, 4),
-                #'organizer': round(organizer, 4),
-                'pay2post': round(pay2post * -1, 4),
-                'eligible_comments': d['eligible_comments'],
-                'eligible_posts': d['eligible_posts'],
-                'eligibility_reason': d['eligibility_reason'],
-                'address': user['address']
-            })
-
-        distribution_summary.sort(key=lambda x: float(x['points']), reverse=True)
+        distribution_summary.sort(key=lambda x: float(x["points"]), reverse=True)
 
         distribution_summary_filename = "distribution_summary"
-        super().save_document_version(distribution_summary, distribution_summary_filename)
+        super().save_document_version(
+            distribution_summary, distribution_summary_filename
+        )
 
-        return super().update_pipeline(pipeline_config, {
-            'distribution_summary': distribution_summary_filename
-        })
+        return super().update_pipeline(
+            pipeline_config,
+            {
+                "distribution_summary": distribution_summary_filename,
+                "distribution_summary_total": distribution_summary_total,
+            },
+        )
